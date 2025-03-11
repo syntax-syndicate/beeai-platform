854f74b13ae6b93d08683bdde96e43a9e575f877
Pavel Bucka <ppenge@gmail.com>
2025-03-11 17:44:38 +0100

    feat(ui): animate BeeAI logo (#345)

--- apps/beeai-ui/src/modules/home/components/GettingStarted.tsx
+++ apps/beeai-ui/src/modules/home/components/GettingStarted.tsx
@@ -17,9 +17,9 @@
 'use client';
 
 import { BREW_INSTALL_BEEAI } from '@i-am-bee/beeai-ui';
-import LogoBeeAI from '#svgs/LogoBeeAI.svg';
 import { Container } from '#components/layouts/Container.tsx';
 import { CopySnippet } from '#components/CopySnippet/CopySnippet.tsx';
+import { LogoBeeAI } from './LogoBeeAI';
 import { GitHubButton } from './GitHubButton';
 import classes from './GettingStarted.module.scss';
 
