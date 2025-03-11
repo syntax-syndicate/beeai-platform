f2e4968511081f935b41a052cc168a5dae072d20
Pavel Bucka <ppenge@gmail.com>
2025-03-11 10:56:29 +0100

    feat(web): add SVG favicon (#333)

--- apps/beeai-web/src/app/layout.tsx
+++ apps/beeai-web/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/Bee.svg" />
+      </head>
       <body>
         <Providers>
           <AppLayout>{children}</AppLayout>
