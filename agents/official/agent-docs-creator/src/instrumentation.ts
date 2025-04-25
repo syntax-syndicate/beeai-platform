916fc91244de444720661e8a89b40b42740cb473
Radek Ježek <radek.jezek@ibm.com>
2025-04-25 11:16:16 +0200

    chore(agents): rewrite agent-docs-creator for new acp
    Signed-off-by: Radek Ježek <radek.jezek@ibm.com>

--- agents/official/agent-docs-creator/src/instrumentation.ts
+++
@@ -1,5 +0,0 @@
-import { BeeAIInstrumentation } from "@arizeai/openinference-instrumentation-beeai";
-import * as beeaiFramework from "beeai-framework";
-
-const beeAIInstrumentation = new BeeAIInstrumentation();
-beeAIInstrumentation.manuallyInstrument(beeaiFramework);
