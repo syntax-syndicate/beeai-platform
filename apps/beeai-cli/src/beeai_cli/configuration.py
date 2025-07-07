6ddb3209b1dfa8e92f123dfeb8374fcf38f165f4
Jan Pokorný <JenomPokorny@gmail.com>
2025-07-07 09:46:34 +0200

    fix(beeai-cli): correctly handle dash in version string
    Signed-off-by: Jan Pokorný <JenomPokorny@gmail.com>

--- apps/beeai-cli/src/beeai_cli/configuration.py
+++ apps/beeai-cli/src/beeai_cli/configuration.py
@@ -2,12 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import functools
+import importlib.metadata
 import pathlib
-from importlib.metadata import version
+import re
 
 import pydantic
 import pydantic_settings
 from pydantic import SecretStr
+
+
+@functools.cache
+def version():
+    # Python strips '-', we need to re-insert it: 1.2.3rc1 -> 1.2.3-rc1
+    return re.sub(r"([0-9])([a-z])", r"\1-\2", importlib.metadata.version("beeai-cli"))
 
 
 @functools.cache
@@ -20,14 +27,10 @@ class Configuration(pydantic_settings.BaseSettings):
     debug: bool = False
     home: pathlib.Path = pathlib.Path.home() / ".beeai"
     agent_registry: pydantic.AnyUrl = (
-        f"https://github.com/i-am-bee/beeai-platform@v{version('beeai-cli')}#path=agent-registry.yaml"
+        f"https://github.com/i-am-bee/beeai-platform@v{version()}#path=agent-registry.yaml"
     )
     admin_password: SecretStr | None = None
 
     @property
     def lima_home(self) -> pathlib.Path:
         return self.home / "lima"
-
-    @property
-    def docker_home(self) -> pathlib.Path:
-        return self.home / "docker"
