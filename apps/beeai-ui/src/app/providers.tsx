8e3fc611c5167423f9e52a83d6f033f567af86f9
Petr Kadlec <petr@puradesign.cz>
2025-07-17 13:20:00 +0200

    fix(ui): broken error toasts and query invalidation (#935)
    Signed-off-by: Petr Kadlec <petr@puradesign.cz>

--- apps/beeai-ui/src/app/providers.tsx
+++ apps/beeai-ui/src/app/providers.tsx
@@ -4,37 +4,33 @@
  */
 
 'use client';
-import { QueryClientProvider } from '@tanstack/react-query';
 import type { PropsWithChildren } from 'react';
 
 import { AppProvider } from '#contexts/App/AppProvider.tsx';
 import { AppConfigProvider } from '#contexts/AppConfig/AppConfigProvider.tsx';
 import { ModalProvider } from '#contexts/Modal/ModalProvider.tsx';
 import { ProgressBarProvider } from '#contexts/ProgressBar/ProgressBarProvider.tsx';
+import { QueryProvider } from '#contexts/QueryProvider/QueryProvider.tsx';
 import { ThemeProvider } from '#contexts/Theme/ThemeProvider.tsx';
 import { ToastProvider } from '#contexts/Toast/ToastProvider.tsx';
 import { RouteTransitionProvider } from '#contexts/TransitionContext/RouteTransitionProvider.tsx';
 
-import { getQueryClient } from './get-query-client';
-
 export default function Providers({ children }: PropsWithChildren) {
-  const queryClient = getQueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <ToastProvider>
       <ProgressBarProvider>
         <ThemeProvider>
           <RouteTransitionProvider>
-            <ToastProvider>
+            <QueryProvider>
               <ModalProvider>
                 <AppConfigProvider>
                   <AppProvider>{children}</AppProvider>
                 </AppConfigProvider>
               </ModalProvider>
-            </ToastProvider>
+            </QueryProvider>
           </RouteTransitionProvider>
         </ThemeProvider>
       </ProgressBarProvider>
-    </QueryClientProvider>
+    </ToastProvider>
   );
 }
